7df3b294347ad44befdb3e9c33e538090b9744d6
Vivek Bhawsar <93441216+VivekBhawsar18@users.noreply.github.com>
2023-08-21 18:55:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,98 +1,59 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { connectToDB, getUserCollection} = require("./db");
+const { connectToDB, getUserCollection } = require("./db");
 // const routes = require("./routes");
-
 
 const app = express();
 app.use(bodyParser.json());
 
-
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
   const userData = {
     email: req.body.email,
     mobileNumber: req.body.mobileNumber,
     password: req.body.password,
-    confirmpassword: req.body.confirmpassword
+    confirmpassword: req.body.confirmpassword,
   };
 
   try {
     const collection = await getUserCollection();
-    const existingUserByEmail = await collection.findOne({ email: userData.email });
-    const existingUserByMobile = await collection.findOne({ mobileNumber: userData.mobileNumber });
+    const existingUserByEmail = await collection.findOne({
+      email: userData.email,
+    });
+    const existingUserByMobile = await collection.findOne({
+      mobileNumber: userData.mobileNumber,
+    });
 
     if (existingUserByEmail) {
       res.send("User email already exists");
     } else if (existingUserByMobile) {
-      res.send("User mobile number already exists");  
-    }else {
+      res.send("User mobile number already exists");
+    } else {
       await collection.insertOne(userData);
       console.log(userData);
       res.status(201).send("User registered successfully");
     }
   } catch (error) {
-    console.error('Error during signup:', error);
+    console.error("Error during signup:", error);
     res.status(500).send("An error occurred during signup");
-
-// app.post("/login", async (req, res) => {
-//   try {
-//     const {emailId, password} = req.body;
-//     if (!emailId){
-//       res.status(400).json({  error:"Email is required." });
-//     }
-
-//     if (!password){
-//       res.status(400).json({  error:"password is required." });
-//     }
-//     const collection= await getUserCollection();
-//     const newUser ={
-//       emailId:req.body.emailId,
-//       password:req.body.password
-//     };
-//     console.log(emailId);
-//     const userEmail = await collection.findOne({emailId : emailId})
-//     const validPassword = await collection.findOne(password)
-//     console.log(userEmail);
-//     if(userEmail !== emailId){
-//       res.status(404).json({message:"Email not matched"});
-//     }
-//       else if(userEmail.validPassword !== validPassword){
-//         res.status(200).json({message:"Login successfull"})
-//       }
-      
-//        else{
-//         res.status(404).json({message:"Password not matched"});
-
-//        }
-    
-//     res.status(201).json(newUser);
-//   } catch (err) {
-//     console.error("error login users:", err);
-//     res.status(500).json({  error:"Failed to login users."});
-//   }
-// });
-
-
-
+  }
+});
 
 app.post("/login", async (req, res) => {
   try {
     const { emailId, password } = req.body;
     if (!emailId) {
       return res.status(400).json({ error: "Email is required." });
-    }else if (!password) {
+    } else if (!password) {
       return res.status(400).json({ error: "Password is required." });
     }
-    
+
     const collection = await getUserCollection();
-    const userEmail = await collection.findOne({ emailId }); 
+    const userEmail = await collection.findOne({ emailId });
 
     if (!userEmail) {
       return res.status(404).json({ message: "Email not found" });
-    }
-
-    else if (userEmail.password !== password) {
+    } else if (userEmail.password !== password) {
       return res.status(401).json({ message: "Password not matched" });
     }
 
